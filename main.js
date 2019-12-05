b7594c98432273591b769c76523a291b60a9467e
aroe <lipton.happy.milktea@gmail.com>
2019-12-05 23:19:28 +0900

    version 1.0

---
+++ main.js
@@ -0,0 +1,52 @@
+let data = decodeURI(location.search).replace('?json=', '');
+
+let presents = JSON.parse(data);
+
+let showPresent = function () {
+    if (presents.length == 0) {
+        alert('抽選は終了しました．')
+        return;
+    }
+    let random = Math.floor(Math.random() * presents.length);
+
+    while (presents[random].number == 0) {
+        random = Math.floor(Math.random() * presents.length);
+    }
+    showBall();
+    setTimeout(() => {
+        document.getElementById('present').innerHTML = presents[random].name;
+    }, 1000);
+    setTimeout(() => {
+        openBall();
+    }, 1000);
+    $('.fixed-bottom').removeClass('display-none')
+    presents[random].number--;
+    if (presents[random].number == 0) {
+        // if (presents.length == 1) {
+        //     presents = [];
+        // }
+        presents.splice(random, 1);
+    }
+
+}
+
+let showBall = function(){
+    $('.ball').addClass('fall');
+}
+
+let openBall = function(){
+    $('#up').addClass('up');
+    $('#down').addClass('down');
+}
+
+let hideBall = function(){
+    $('.ball').addClass('display-none');
+    setTimeout(() => {
+        $('.ball').removeClass('display-none');
+    }, 300);
+    $('.ball').removeClass('fall');
+    $('#up').removeClass('up');
+    $('#down').removeClass('down');
+    document.getElementById('present').innerHTML = '';
+    $('.fixed-bottom').addClass('display-none')
+}
